dd87447657d16a1566ec036d79877862bf9f3b10
jimbn <nguyenb.jim@gmail.com>
2022-06-26 21:51:39 -0700

    add image-upload file

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const userRoutes = require('./routes/user-routes');
+const userRoutes = require('./routes/user-routes');
+const imageRoutes = require('./routes/image-upload'); 
 
 // express middleware, used to be bodyparser
 app.use(express.json());
@@ -14,8 +16,10 @@ if (process.env.NODE_ENV === 'production') {
 
 // app.use(require('./routes'));
 app.use('/api/', userRoutes);
+app.use('/api/', imageRoutes); // add this route for image upload
 
 // Start the API server
 app.listen(PORT, () =>
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`),
 );
+
